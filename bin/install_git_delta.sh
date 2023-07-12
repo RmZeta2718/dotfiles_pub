89bba11888917524c8e1bfcbce7bd2c13e07a637
WangJie <2740469261@qq.com>
2023-07-12 10:24:08 +0800

    feat: curl only if not exist (install delta)

--- bin/install_git_delta.sh
+++ bin/install_git_delta.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+set -e
+
 # Specify the URL of the .deb file to download
 deb_url="https://github.com/dandavison/delta/releases/download/0.16.5/git-delta_0.16.5_amd64.deb"
 
 # Specify the desired filename for the downloaded .deb file
 deb_file="$HOME/$(basename "$deb_url")"
 
-wget "$deb_url" -O "$deb_file" && (sudo dpkg -i "$deb_file"; rm "$deb_file")
+if [ ! -f "$deb_file" ]; then
+    wget "$deb_url" -O "$deb_file"
+else
+    echo "Using existing $deb_file"
+fi
+
+sudo dpkg -i "$deb_file"
+rm "$deb_file"
