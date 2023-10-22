96d4e05ca9680d41f4b5b2751e62268d9a58fb67
WangJie <2740469261@qq.com>
2023-10-22 19:40:54 +0800

    feat: proxy rename

--- shell/proxy.sh
+++ shell/proxy.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-proxy_grep() {
+pg() {
     export | grep proxy
 }
 
-proxy_on() {
+po() {
     local ip
-    ip=${1:-"127.0.0.1"}  # ip=$1, with default value
+    ip=${1:-"127.0.0.1"} # ip=$1, with default value
     # cannot export to https://ip:port, must be http://
     export https_proxy="http://${ip}:7890" http_proxy="http://${ip}:7890" all_proxy="socks5://${ip}:7891"
 }
 
-proxy_off() {
+pof() {
     unset https_proxy http_proxy all_proxy
 }
 
-alias proxy_test="curl -I google.com"
+pt() {
+    local url
+    url=${1:-"www.google.com"}
+    curl -I "$url"
+}
 
+alias pth="pt https://huggingface.co"
