50e72de5252be929d880009da254fcfc1d147bc1
WangJie <2740469261@qq.com>
2025-07-07 14:16:01 +0800

    feat(shell): better po

--- shell/proxy.sh
+++ shell/proxy.sh
@@ -7,8 +7,13 @@ pg() {
 po() {
     local ip
     ip=${1:-"127.0.0.1"} # ip=$1, with default value
+    if [ "$ip" = "_" ]; then
+        ip="127.0.0.1"
+    fi
+    port=${2:-7890}
+    port2=${3:-$((port + 1))}
     # cannot export to https://ip:port, must be http://
-    export https_proxy="http://${ip}:7890" http_proxy="http://${ip}:7890" all_proxy="socks5://${ip}:7891"
+    export https_proxy="http://${ip}:${port}" http_proxy="http://${ip}:${port}" all_proxy="socks5://${ip}:${port2}"
 }
 
 pof() {
